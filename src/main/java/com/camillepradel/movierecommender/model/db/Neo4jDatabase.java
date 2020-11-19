5083637aa18f8953a771e510eee400034a09499e
Ancelin GLORIES <ancelin.glories@gmail.com>
2020-11-19 21:30:32 +0100

    Implémentation de deux méthodes

--- src/main/java/com/camillepradel/movierecommender/model/db/Neo4jDatabase.java
+++ src/main/java/com/camillepradel/movierecommender/model/db/Neo4jDatabase.java
@@ -17,6 +17,7 @@ import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
 import org.neo4j.driver.Transaction;
 import org.neo4j.driver.TransactionWork;
+import static org.neo4j.driver.Values.parameters;
 
 public class Neo4jDatabase extends AbstractDatabase {
 
@@ -78,24 +79,102 @@ public class Neo4jDatabase extends AbstractDatabase {
     @Override
     public List<Movie> getMoviesRatedByUser(int userId) {
         // TODO: write query to retrieve all movies rated by user with id userId
-        List<Movie> movies = new LinkedList<Movie>();
+        /*List<Movie> movies = new LinkedList<Movie>();
         Genre genre0 = new Genre(0, "genre0");
         Genre genre1 = new Genre(1, "genre1");
         Genre genre2 = new Genre(2, "genre2");
         movies.add(new Movie(0, "Titre 0", Arrays.asList(new Genre[]{genre0, genre1})));
         movies.add(new Movie(3, "Titre 3", Arrays.asList(new Genre[]{genre0, genre1, genre2})));
-        return movies;
+        return movies;*/
+        
+        HashMap<Integer, Genre> allGenres = new HashMap<Integer,Genre>();
+        List<Movie> allMovies = new LinkedList<Movie>();
+        
+        /// Todo : Change LinkedList
+        try ( Session session = driver.session() )
+        {
+            return session.readTransaction(tx -> {
+                Result rs = tx.run(
+                    "MATCH (u1:User {id:$userId})-[:RATED]->(m:Movie)-[:CATEGORIZED_AS]->(g:Genre)\n" +
+                    "RETURN m.id, m.title, g.id, g.name",
+                    parameters("userId", userId)
+                );
+                while(rs.hasNext()) {
+                    Record aRec = rs.next();
+                    Integer movieId = aRec.get(0).asInt();
+                    String movieName = aRec.get(1).asString();
+                    Integer genreId = aRec.get(2).asInt();
+                    String genreTitle = aRec.get(3).asString();
+                    
+                    // Ajout du genre si non-existant
+                    if (!allGenres.containsKey(genreId)) {
+                        allGenres.put(genreId, new Genre(genreId, genreTitle));
+                    }
+                    
+                    // Ajoute du film si inexistant
+                    if(!checkIfMovieExists(allMovies, movieId)) {
+                        List<Genre> movieGenres = new LinkedList<>();
+                        movieGenres.add(allGenres.get(genreId));
+                        allMovies.add(new Movie(movieId, movieName, movieGenres));
+                    } else {
+                        // Si existant il faut ajouter un autre genre
+                        int movieIndex = getMovieContainingId(allMovies, movieId);
+                        if (movieIndex > -1) {
+                            Movie aMovie = allMovies.get(movieIndex);
+                            aMovie.getGenres().add(allGenres.get(genreId));
+                        } else {
+                            System.err.println("Impossible mais pourquoi pas : Le film n'a pas été trouvé...");
+                        }
+                    }
+                }
+                return allMovies;
+            });
+        }
     }
 
     @Override
     public List<Rating> getRatingsFromUser(int userId) {
         // TODO: write query to retrieve all ratings from user with id userId
-        List<Rating> ratings = new LinkedList<Rating>();
+        /*List<Rating> ratings = new LinkedList<Rating>();
         Genre genre0 = new Genre(0, "genre0");
         Genre genre1 = new Genre(1, "genre1");
         ratings.add(new Rating(new Movie(0, "Titre 0", Arrays.asList(new Genre[]{genre0, genre1})), userId, 3));
         ratings.add(new Rating(new Movie(2, "Titre 2", Arrays.asList(new Genre[]{genre1})), userId, 4));
-        return ratings;
+        return ratings;*/
+        
+        HashMap<Integer, Genre> allGenres = getAllGenres();
+        List<Movie> allMovies = new LinkedList<Movie>();
+        List<Rating> allRatings = new LinkedList<Rating>();
+        
+        /// Todo : Change LinkedList
+        try ( Session session = driver.session() )
+        {
+            return session.readTransaction(tx -> {
+                Result rs = tx.run(
+                    "MATCH (u1:User {id:$userId})-[r:RATED]->(m:Movie)-[:CATEGORIZED_AS]->(g:Genre)\n" +
+                    "RETURN m.id, m.title, collect(g.id), r.note",
+                    parameters("userId", userId)
+                );
+                while(rs.hasNext()) {
+                    Record aRec = rs.next();
+                    Integer movieId = aRec.get(0).asInt();
+                    String movieName = aRec.get(1).asString();
+                    String genreIds = aRec.get(2).toString();
+                    Integer rating = aRec.get(3).asInt();
+                    
+                    // Ajout du genre pour le film correspondant
+                    String IdsExtracted = genreIds.substring(genreIds.indexOf("[")+1, genreIds.indexOf("]"));
+                    List<Genre> movieGenres = new LinkedList<>();
+                    for (String genreId : IdsExtracted.split(", ")) {
+                        movieGenres.add(allGenres.get(Integer.parseInt(genreId)));
+                    }
+                    
+                    // Des genres dans le film qui est ajouté dans Rating avec l'ID user et la note associé
+                    allRatings.add(new Rating(new Movie(movieId, movieName, movieGenres), userId, rating));
+                }
+                return allRatings;
+            });
+        }
     }
 
     @Override
@@ -130,11 +209,13 @@ public class Neo4jDatabase extends AbstractDatabase {
         return recommendations;
     }
     
+    /* ----- HELPERS ----- */
+    
     private HashMap<Integer, Genre> getAllGenres() {
         try ( Session session = driver.session() )
         {
             return session.readTransaction(tx -> {
-                HashMap<Integer, Genre> allGenres = new HashMap<>();
+                HashMap<Integer, Genre> allGenres = new HashMap<Integer, Genre>();
                 Result rs = tx.run(
                     "MATCH (g:Genre)\n" +
                     "RETURN g.id, g.name"
